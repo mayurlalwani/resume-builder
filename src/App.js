81d8fd13c1eadf82751635f2d7f7a8eb4cbdec05
mayurlalwani <mayur_lalwani4@ymail.com>
2021-01-17 13:28:26 +0530

    Setup google authentication using firebase in frontend

--- src/App.js
+++ src/App.js
@@ -1,5 +1,41 @@
+import React, { useState, useEffect } from "react";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import firebase from "firebase";
+import firebaseAuth from "./config/firebaseConfig";
+
 function App() {
-  return <div className="App"></div>;
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  const [userId, setUserId] = useState("");
+  firebaseAuth.auth().onAuthStateChanged((user) => {
+    return user
+      ? setIsSignedIn(true) && setUserId(user.uid)
+      : setIsSignedIn(false);
+  });
+
+  const firebaseConfig = {
+    signInFlow: "popup",
+    signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+    callbacks: {
+      signInSuccess: () => false,
+    },
+  };
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      setIsSignedIn(!!user);
+      if (user.uid !== "") {
+        setUserId(user.uid);
+      }
+    });
+  }, []);
+  return (
+    <div>
+      {isSignedIn ? <h1>Signed In</h1> : <h1>Not signed In</h1>}
+      <StyledFirebaseAuth
+        uiConfig={firebaseConfig}
+        firebaseAuth={firebase.auth()}
+      />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const firebaseConfig = firebase.initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_DATABASE_URL,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSANGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+});
+
+try {
+  firebase.initializeApp(firebaseConfig);
+} catch (err) {
+  if (!/already exists/.test(err.message)) {
+    console.error("Firebase initialization error", err.stack);
+  }
+}
+
+const firebaseAuth = firebase;
+
+export default firebaseAuth;
