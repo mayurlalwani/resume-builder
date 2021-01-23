58ec49276dda83bbbc5135c13e1299c0068aa8fa
mayurlalwani <mayur_lalwani4@ymail.com>
2021-01-23 22:10:33 +0530

    Set panels in left sidebar

--- src/components/LeftSideBar.js
+++ src/components/LeftSideBar.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Collapse } from "antd";
 import "antd/dist/antd.css";
 import TextField from "@material-ui/core/TextField";
+import { Button } from "antd";
 
 const { Panel } = Collapse;
 
@@ -17,10 +18,17 @@ const text = `
 const LeftSideBar = () => {
   const [fullName, setFullName] = useState("");
   const [dateOfBirth, setDateOfBirth] = useState("");
+  const [expPanelClicked, setAddExperiencePanelClicked] = useState(false);
+  const [expPanelId, setExpPanelId] = useState(0);
   const handleChange = (e) => {
     setFullName(e.target.value);
   };
+  const [experienceValues, setExperienceValues] = useState([{}]);
 
+  const handleChangeExperienceValues = (e) => {
+    setExperienceValues([{ [e.target.name]: e.target.value }]);
+  };
+  console.log(experienceValues);
   const personalInfoLabels = [
     {
       label: "Name",
@@ -44,73 +52,192 @@ const LeftSideBar = () => {
     },
   ];
 
-  const experienceInfoLabelss = [
+  const experienceInfoLabels = [
     {
+      id: 1,
       label: "Company Name",
       placeholder: "Full Name",
+      name: "company",
     },
     {
+      id: 2,
       label: "Location",
       placeholder: "Location",
+      name: "location",
     },
     {
+      id: 3,
       label: "Job Title",
       placeholder: "Job Title",
+      name: "jobTitle",
     },
     {
+      id: 4,
       label: "Start Date",
       placeholder: "Start Date",
     },
     {
+      id: 5,
       label: "End Date",
       placeholder: "End Date",
+    },
+    {
+      id: 6,
+      label: "Description",
+      placeholder: "Description",
+    },
+  ];
+
+  const educationLabels = [
+    {
+      id: 1,
+      label: "College Name",
+      placeholder: "College Name",
+    },
+    {
+      id: 2,
+      label: "Location",
+      placeholder: "Location",
+    },
+    {
+      id: 3,
+      label: "Degree",
+      placeholder: "Degree",
+    },
+    {
+      id: 4,
+      label: "Start Date",
+      placeholder: "Start Date",
+    },
+    {
+      id: 5,
+      label: "End Date",
+      placeholder: "End Date",
+    },
+  ];
+
+  const projectsLabels = [
+    {
+      label: "Project Name",
+      placeholder: "Project Name",
     },
     {
       label: "Description",
       placeholder: "Description",
     },
   ];
+  const skillsLabels = [
+    {
+      label: "Programming Languages",
+      placeholder: "Programming Languages",
+    },
+    {
+      label: "Tools & Technologies",
+      placeholder: "Tools & Technologies",
+    },
+  ];
+  const awardsLabel = [
+    {
+      label: "Awards",
+      placeholder: "Awards",
+    },
+  ];
+
+  const addExperiencePanel = () => {
+    setAddExperiencePanelClicked(true);
+    setExpPanelId(expPanelId + 1);
+  };
   return (
-    <Collapse
-      defaultActiveKey={["1"]}
-      onChange={callback}
-      className="resume-heading-section"
-    >
-      <Panel header="Personal Info" key="1">
-        {personalInfoLabels.map((personalInfo) => (
-          <TextField
-            id="standard-multiline-flexible"
-            label={personalInfo.label}
-            placeholder={personalInfo.placeholder}
-            onChange={handleChange}
-            className="input-value"
-          />
-        ))}
-      </Panel>
-      <Panel header="Experience" key="2">
-        {experienceInfoLabelss.map((experienceInfo) => (
-          <TextField
-            id="standard-multiline-flexible"
-            label={experienceInfo.label}
-            placeholder={experienceInfo.placeholder}
-            onChange={handleChange}
-            className="input-value"
-          />
-        ))}
-      </Panel>
-      <Panel header="This is panel header 3" key="3">
-        <p>{text}</p>
-      </Panel>
-      <Panel header="This is panel header 4" key="4">
-        <p>{text}</p>
-      </Panel>
-      <Panel header="This is panel header 5" key="5">
-        <p>{text}</p>
-      </Panel>
-      <Panel header="This is panel header 6" key="6">
-        <p>{text}</p>
-      </Panel>
-    </Collapse>
+    <div className="left-sidebar">
+      <Collapse
+        // defaultActiveKey={["1"]}
+        onChange={callback}
+        className="resume-heading-section"
+      >
+        <Panel header="Personal Info" key="1">
+          {personalInfoLabels.map((personalInfo) => (
+            <TextField
+              id="standard-multiline-flexible-1"
+              label={personalInfo.label}
+              placeholder={personalInfo.placeholder}
+              onChange={handleChange}
+              className="input-value"
+            />
+          ))}
+        </Panel>
+
+        <Panel header="Experience" key="2">
+          {expPanelClicked && (
+            <Collapse
+              defaultActiveKey={["33"]}
+              onChange={callback}
+              className="resume-heading-section"
+            >
+              <Panel header={`Experience #${expPanelId}`} key="11">
+                {experienceInfoLabels.map((experienceInfo) => (
+                  <TextField
+                    id={`standard-multiline-flexible-3${experienceInfo.id}`}
+                    label={experienceInfo.label}
+                    name={experienceInfo.name}
+                    placeholder={experienceInfo.placeholder}
+                    onChange={handleChangeExperienceValues}
+                    className="input-value"
+                  />
+                ))}
+              </Panel>
+            </Collapse>
+          )}
+          <Button type="primary" size={"large"} onClick={addExperiencePanel}>
+            Add
+          </Button>
+        </Panel>
+
+        <Panel header="Education" key="3">
+          {educationLabels.map((educationInfo) => (
+            <TextField
+              id="standard-multiline-flexible"
+              label={educationInfo.label}
+              placeholder={educationInfo.placeholder}
+              onChange={handleChange}
+              className="input-value"
+            />
+          ))}
+        </Panel>
+        <Panel header="Projects" key="4">
+          {projectsLabels.map((educationInfo) => (
+            <TextField
+              id="standard-multiline-flexible"
+              label={educationInfo.label}
+              placeholder={educationInfo.placeholder}
+              onChange={handleChange}
+              className="input-value"
+            />
+          ))}
+        </Panel>
+        <Panel header="Skills" key="5">
+          {skillsLabels.map((educationInfo) => (
+            <TextField
+              id="standard-multiline-flexible"
+              label={educationInfo.label}
+              placeholder={educationInfo.placeholder}
+              onChange={handleChange}
+              className="input-value"
+            />
+          ))}
+        </Panel>
+        <Panel header="Awards and Achievemnts" key="6">
+          {awardsLabel.map((educationInfo) => (
+            <TextField
+              id="standard-multiline-flexible"
+              label={educationInfo.label}
+              placeholder={educationInfo.placeholder}
+              onChange={handleChange}
+              className="input-value"
+            />
+          ))}
+        </Panel>
+      </Collapse>
+    </div>
   );
 };
 
