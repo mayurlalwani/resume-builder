c99afea2d88dd302d96bf3b83c740c98485f84fe
mayurlalwani <mayur_lalwani4@ymail.com>
2021-02-07 20:12:48 +0530

    Config file for resume details

---
+++ src/components/expLabels.js
@@ -0,0 +1,40 @@
+export const ExperienceDetailsConfig = [
+  {
+    id: 1,
+    details: [
+      {
+        label: "Company Name",
+        placeholder: "Full Name",
+        name: "company",
+        value: "",
+      },
+      {
+        label: "Location",
+        placeholder: "Location",
+        name: "location",
+        value: "",
+      },
+      {
+        label: "Job Title",
+        placeholder: "Job Title",
+        name: "jobTitle",
+        value: "",
+      },
+      {
+        label: "Start Date",
+        placeholder: "",
+        value: "",
+      },
+      {
+        label: "End Date",
+        placeholder: "End Date",
+        value: "",
+      },
+      {
+        label: "Description",
+        placeholder: "",
+        value: "",
+      },
+    ],
+  },
+];
